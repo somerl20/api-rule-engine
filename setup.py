e3950aac3d6ccd690b6e8ef2b97b7c873574e3d6
omerlondon <omer.london@minereye.com>
2022-03-01 17:01:47 +0200

    new repo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 import setuptools
 
-with open("../json-rules/README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
