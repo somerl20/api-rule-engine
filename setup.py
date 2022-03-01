031adf21f20833476c187746145970636dcf967e
omerlondon <omer.london@minereye.com>
2022-03-01 17:30:52 +0200

    new repo

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -7,12 +6,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='api-rule-engine',
     version='0.0.1',
+    scripts=['api-rule-engine'],
     author='Omer London',
     description='rule engine API',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/somerl20/api-rule-engine.git',
+    project_urls={},
     license='GNU',
-    packages=['api-rule-engine'],
+    packages=setuptools.find_packages(),
     install_requires=['requests>=2.25.1'],
 )
