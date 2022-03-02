21cc8ca924b6e3f36324e163bbce83b17a4076ee
omerlondon <omer.london@minereye.com>
2022-03-02 14:18:32 +0200

    new repo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='api-rule-engine',
-    version='0.0.2',
+    version='0.0.21',
     scripts=['api-rule-engine'],
     author='Omer London',
     description='rule engine API',
